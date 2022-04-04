5ed7f3b1b11ba019efff93d9e2a601501886ee71
Reski Mulud Muchamad <63949402+reskimulud@users.noreply.github.com>
2022-04-04 22:22:58 +0700

    update project with create action on widget

--- MyWidgets/app/src/main/java/com/mankart/mywidgets/RandomNumberWidget.kt
+++ MyWidgets/app/src/main/java/com/mankart/mywidgets/RandomNumberWidget.kt
@@ -1,8 +1,12 @@
 package com.mankart.mywidgets
 
+import android.annotation.SuppressLint
+import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.content.Intent
+import android.os.Build
 import android.widget.RemoteViews
 
 /**
@@ -27,18 +31,52 @@ class RandomNumberWidget : AppWidgetProvider() {
     override fun onDisabled(context: Context) {
         // Enter relevant functionality for when the last widget is disabled
     }
-}
 
-internal fun updateAppWidget(
-    context: Context,
-    appWidgetManager: AppWidgetManager,
-    appWidgetId: Int
-) {
-    val lastUpdate = "Random: " + NumberGenerator.generate(100)
-    // Construct the RemoteViews object
-    val views = RemoteViews(context.packageName, R.layout.random_number_widget)
-    views.setTextViewText(R.id.appwidget_text, lastUpdate)
+    @SuppressLint("RemoteViewLayout")
+    override fun onReceive(context: Context, intent: Intent) {
+        super.onReceive(context, intent)
+        if (WIDGET_CLICK == intent.action) {
+            val appWidgetManager: AppWidgetManager = AppWidgetManager.getInstance(context)
+            val views = RemoteViews(context.packageName, R.layout.random_number_widget)
+            val lastUpdate = "Random: " + NumberGenerator.generate(100)
+            val appWidgetId = intent.getIntExtra(WIDGET_ID_EXTRA, 0)
 
-    // Instruct the widget manager to update the widget
-    appWidgetManager.updateAppWidget(appWidgetId, views)
+            views.setTextViewText(R.id.appwidget_text, lastUpdate)
+            appWidgetManager.updateAppWidget(appWidgetId, views)
+        }
+    }
+
+    @SuppressLint("RemoteViewLayout")
+    private fun updateAppWidget(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetId: Int
+    ) {
+        val lastUpdate = "Random: " + NumberGenerator.generate(100)
+        // Construct the RemoteViews object
+        val views = RemoteViews(context.packageName, R.layout.random_number_widget)
+        views.setTextViewText(R.id.appwidget_text, lastUpdate)
+        views.setOnClickPendingIntent(R.id.btn_click, getPendingIntentSelf(context, appWidgetId, WIDGET_CLICK))
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views)
+    }
+
+    private fun getPendingIntentSelf(context: Context, appWidgetId: Int, action: String): PendingIntent {
+        val intent = Intent(context, RandomNumberWidget::class.java)
+        intent.action = action
+        intent.putExtra(WIDGET_ID_EXTRA, appWidgetId)
+        return PendingIntent.getBroadcast(context, appWidgetId, intent,
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_MUTABLE
+            } else {
+                0
+            }
+        )
+    }
+
+    companion object {
+        private const val WIDGET_CLICK = "android.appwidget.action.APPWIDGET_UPDATE"
+        private const val WIDGET_ID_EXTRA = "widget_id_extra"
+    }
 }
