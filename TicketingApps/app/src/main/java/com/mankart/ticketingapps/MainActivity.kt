82878d442ac2c1ed0f28b7433f2a347106b6a477
Reski Mulud Muchamad <63949402+reskimulud@users.noreply.github.com>
2022-04-04 18:01:06 +0700

    add custom view (seat view) to layout

--- TicketingApps/app/src/main/java/com/mankart/ticketingapps/MainActivity.kt
+++ TicketingApps/app/src/main/java/com/mankart/ticketingapps/MainActivity.kt
@@ -2,10 +2,28 @@ package com.mankart.ticketingapps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.widget.Toast
+import com.mankart.ticketingapps.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        supportActionBar?.hide()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnFinish.setOnClickListener {
+            binding.seatsView.seat?.let {
+                Toast.makeText(this, "Selected Seats: ${it.name}", Toast.LENGTH_SHORT).show()
+            } ?: run {
+                Toast.makeText(this, "Please select seats", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
