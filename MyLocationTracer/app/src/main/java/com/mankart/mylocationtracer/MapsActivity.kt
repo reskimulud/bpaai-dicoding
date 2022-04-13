c69ece4e6f22024341b9c8e705f1946c6bad3a52
Reski Mulud Muchamad <63949402+reskimulud@users.noreply.github.com>
2022-04-13 22:58:54 +0700

    update and finishing project MyLocationTracer

--- MyLocationTracer/app/src/main/java/com/mankart/mylocationtracer/MapsActivity.kt
+++ MyLocationTracer/app/src/main/java/com/mankart/mylocationtracer/MapsActivity.kt
@@ -1,13 +1,19 @@
 package com.mankart.mylocationtracer
 
 import android.Manifest
+import android.content.IntentSender
 import android.content.pm.PackageManager
+import android.graphics.Color
 import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
+import android.util.Log
 import android.widget.Toast
+import androidx.activity.result.IntentSenderRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
+import com.google.android.gms.common.api.ResolvableApiException
 
 import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -15,15 +21,22 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.PolylineOptions
 import com.mankart.mylocationtracer.databinding.ActivityMapsBinding
+import java.util.concurrent.TimeUnit
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
-
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private lateinit var locationRequest: LocationRequest
+    private lateinit var locationCallback: LocationCallback
+    private lateinit var boundsBuilder: LatLngBounds.Builder
+
+    private var isTracking: Boolean = false
+    private var allLatLng = ArrayList<LatLng>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -56,13 +69,135 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 //        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
 //        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
 
-        getMyLastLocation()
+//        getMyLastLocation()
         createLocationRequest()
+        createLocationCallback()
+
+        binding.btnStart.setOnClickListener {
+            if (!isTracking) {
+                clearMaps()
+                updateTrackingStatus(true)
+                startLocationUpdate()
+            } else {
+                updateTrackingStatus(false)
+                stopLocationUpdate()
+            }
+        }
+    }
+
+    private fun clearMaps() {
+        mMap.clear()
+        allLatLng.clear()
+        boundsBuilder = LatLngBounds.Builder()
+    }
+
+    private fun stopLocationUpdate() {
+        fusedLocationClient.removeLocationUpdates(locationCallback)
+    }
+
+    private fun startLocationUpdate() {
+        try {
+            fusedLocationClient.requestLocationUpdates(
+                locationRequest,
+                locationCallback,
+                Looper.getMainLooper()
+            )
+        } catch (exception: SecurityException) {
+            Log.e(TAG, "error: ${exception.message}")
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (isTracking) {
+            startLocationUpdate()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        stopLocationUpdate()
+    }
+
+    private fun createLocationCallback() {
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                for (location in locationResult.locations) {
+                    Log.d(TAG, "onLocationResult: ${location.latitude}, ${location.longitude}")
+                    val latLng = LatLng(location.latitude, location.longitude)
+
+                    // draw polyline
+                    allLatLng.add(latLng)
+                    mMap.addPolyline(
+                        PolylineOptions()
+                            .addAll(allLatLng)
+                            .color(Color.CYAN)
+                            .width(5f)
+                    )
+
+                    // boundaries
+                    boundsBuilder.include(latLng)
+                    val bounds: LatLngBounds = boundsBuilder.build()
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngBounds(bounds, 100))
+                }
+            }
+        }
+    }
+
+    private fun updateTrackingStatus(status: Boolean) {
+        isTracking = status
+        if (isTracking) {
+            binding.btnStart.text = getString(R.string.stop_running)
+        } else {
+            binding.btnStart.text = getString(R.string.start_running)
+        }
     }
 
     private fun createLocationRequest() {
-        TODO("Not yet implemented")
+        locationRequest = LocationRequest.create().apply {
+            interval = TimeUnit.SECONDS.toMillis(1)
+            maxWaitTime = TimeUnit.SECONDS.toMillis(1)
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+
+        val builder = LocationSettingsRequest.Builder()
+            .addLocationRequest(locationRequest)
+        val client = LocationServices.getSettingsClient(this)
+        client.checkLocationSettings(builder.build())
+            .addOnSuccessListener {
+                getMyLastLocation()
+            }
+            .addOnFailureListener { exception ->
+                if (exception is ResolvableApiException) {
+                    // Location settings are not satisfied, but this can be fixed
+                    // by showing the user a dialog.
+                    try {
+                        // Show the dialog by calling startResolutionForResult(),
+                        // and check the result in onActivityResult().
+                        resolutionLauncher.launch(
+                            IntentSenderRequest.Builder(exception.resolution).build()
+                        )
+                    } catch (sendEx: IntentSender.SendIntentException) {
+                        // Ignore the error.
+                        Toast.makeText(this, sendEx.message, Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
     }
+
+    private val resolutionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.StartIntentSenderForResult()
+        ) { result ->
+            when (result.resultCode) {
+                RESULT_OK -> Log.i(TAG, "User agreed to make required location settings changes.")
+                RESULT_CANCELED -> Toast.makeText(
+                    this,
+                    "Use GPS to use this app.",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
 
     private val requestPermissionLauncher =
         registerForActivityResult(
@@ -110,5 +245,14 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                 .title(getString(R.string.start_point))
         )
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(startLocation, 15f))
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        clearMaps()
+    }
+
+    companion object {
+        private const val TAG = "MapsActivity"
     }
 }
