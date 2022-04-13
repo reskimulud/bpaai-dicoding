0f997f0d44c719dbe8847f506cc981e21981de2c
Reski Mulud Muchamad <63949402+reskimulud@users.noreply.github.com>
2022-04-13 13:39:07 +0700

    update project and add custom marker and get my location

--- MyGoogleMaps/app/src/main/java/com/mankart/mygooglemaps/MapsActivity.kt
+++ MyGoogleMaps/app/src/main/java/com/mankart/mygooglemaps/MapsActivity.kt
@@ -1,16 +1,28 @@
 package com.mankart.mygooglemaps
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.content.res.Resources
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.ColorInt
+import androidx.annotation.DrawableRes
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
+import androidx.core.graphics.drawable.DrawableCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.*
 import com.mankart.mygooglemaps.databinding.ActivityMapsBinding
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
@@ -56,6 +68,63 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap.addMarker(MarkerOptions().position(myHouse).title("My House").snippet("Sukabumi, Indonesia"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(myHouse))
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(myHouse, 15f))
+
+        mMap.setOnMapLongClickListener { latlng ->
+            mMap.addMarker(
+                MarkerOptions()
+                    .position(latlng)
+                    .title("New Marker")
+                    .snippet("Lat: ${latlng.latitude}, Lng: ${latlng.longitude}")
+                    .icon(
+                        vectorToBitmap(
+                            R.drawable.ic_android,
+                            Color.parseColor("#3DDC84")
+                        )
+                    )
+            )
+        }
+
+        getMyLocation()
+        setMapStyle()
+    }
+
+    private fun setMapStyle() {
+        try {
+            val success =
+                mMap.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(
+                        this,
+                        R.raw.map_style
+                    )
+                )
+
+            if (!success) {
+                Log.e("MapsActivity", "Style parsing failed.")
+            }
+        } catch (e: Resources.NotFoundException) {
+            Log.e("MapsActivity", "Can't find style. Error: ", e)
+        }
+    }
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                getMyLocation()
+            }
+        }
+
+    private fun getMyLocation() {
+        if (ContextCompat.checkSelfPermission(
+                this.applicationContext,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            mMap.isMyLocationEnabled = true
+        } else {
+            requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -82,6 +151,24 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                 true
             }
             else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun vectorToBitmap(@DrawableRes id: Int, @ColorInt color: Int): BitmapDescriptor {
+        val vectorDrawable = ResourcesCompat.getDrawable(resources, id, null)
+        return if (vectorDrawable != null) {
+            val bitmap = Bitmap.createBitmap(
+                vectorDrawable.intrinsicWidth,
+                vectorDrawable.intrinsicHeight, Bitmap.Config.ARGB_8888
+            )
+            val canvas = Canvas(bitmap)
+            vectorDrawable.setBounds(0, 0, canvas.width, canvas.height)
+            DrawableCompat.setTint(vectorDrawable, color)
+            vectorDrawable.draw(canvas)
+            BitmapDescriptorFactory.fromBitmap(bitmap)
+        } else {
+            Log.e("MainActivity", "vectorToBitmap: drawable not found")
+            BitmapDescriptorFactory.defaultMarker()
         }
     }
 }
