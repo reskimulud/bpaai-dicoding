06e3c5f025e62ce86a95de3bc2704af02c0579e1
Reski Mulud Muchamad <63949402+reskimulud@users.noreply.github.com>
2022-04-15 20:48:18 +0700

    create project to implement relation on room database

---
+++ MyStudentData/app/src/main/java/com/dicoding/mystudentdata/database/StudentDao.kt
@@ -0,0 +1,24 @@
+package com.dicoding.mystudentdata.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface StudentDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertStudent(student: List<Student>)
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertUniversity(university: List<University>)
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertCourse(course: List<Course>)
+
+    @Query("SELECT * from student")
+    fun getAllStudent(): LiveData<List<Student>>
+
+    @Transaction
+    @Query("SELECT * from student")
+    fun getAllStudentAndUniversity(): LiveData<List<StudentAndUniversity>>
+
+}
