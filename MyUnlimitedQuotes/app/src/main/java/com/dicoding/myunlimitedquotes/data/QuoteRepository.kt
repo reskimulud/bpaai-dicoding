44d955d5241f0c319831e3bf6c8121415473dfd1
Reski Mulud Muchamad <63949402+reskimulud@users.noreply.github.com>
2022-04-17 03:45:22 +0700

    implementing unlimited scroll with paging 3

--- MyUnlimitedQuotes/app/src/main/java/com/dicoding/myunlimitedquotes/data/QuoteRepository.kt
+++ MyUnlimitedQuotes/app/src/main/java/com/dicoding/myunlimitedquotes/data/QuoteRepository.kt
@@ -1,11 +1,21 @@
 package com.dicoding.myunlimitedquotes.data
 
+import androidx.lifecycle.LiveData
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import androidx.paging.liveData
 import com.dicoding.myunlimitedquotes.database.QuoteDatabase
 import com.dicoding.myunlimitedquotes.network.ApiService
 import com.dicoding.myunlimitedquotes.network.QuoteResponseItem
 
 class QuoteRepository(private val quoteDatabase: QuoteDatabase, private val apiService: ApiService) {
-    suspend fun getQuote(): List<QuoteResponseItem> {
-        return apiService.getQuote(1, 5)
+    fun getQuote(): LiveData<PagingData<QuoteResponseItem>> {
+        return Pager(
+            config = PagingConfig(
+                pageSize = 5
+            ),
+            pagingSourceFactory = { QuotePagingSource(apiService) }
+        ).liveData
     }
 }
